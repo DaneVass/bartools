b07dd20643060dc3cd1778cbf9ed731a1afcba26
Henrietta Holze <henrietta.holze@petermac.org>
2023-11-02 11:38:18 +1100

    update plotBarcodeStackedBar

--- R/plotBarcodeStackedBar.R
+++ R/plotBarcodeStackedBar.R
@@ -5,7 +5,9 @@
 #' @param counts dataframe containing raw counts of barcodes. Expects barcodes as row names and samples as columns. Alternatively, DGE object.
 #' @param n_barcodes number of most frequent barcodes to show in legend
 #' @param seed_colors seed for sampling colors
-#' @param samples samples to plot
+#' @param samples samples to plot. Default is all samples.
+#' @param order_samples samples to order barcodes by. Default all.
+#' @param alpha_low_freq Alpha of barcodes not in top n barcodes.
 #'
 #' @return Returns a stacked bar plot of barcode frequencies within samples
 #'
@@ -18,17 +20,29 @@ plotBarcodesStackedBar <-
   function(counts,
            n_barcodes = 10,
            seed_colors = 1,
-           samples = NULL) {
+           samples = NULL,
+           order_samples = NULL,
+           alpha_low_freq = 1) {
     if (methods::is(counts)[1] == "DGEList") {
-      # if no samplesheet is provided, extract it from DGE object
       counts <- as.data.frame(counts$counts)
     } else {
       counts <- as.data.frame(counts)
     }
 
-    if (!is.null(samples)) {
-      counts <- counts[, samples]
+    if (is.null(samples)) {
+      samples <- colnames(counts)
     }
+
+    counts <- counts[, samples]
+
+    if (!is.null(order_samples)) {
+      if (!all(order_samples %in% samples)) {
+        stop("samples to order by are not present or among selected samples")
+      }
+    } else {
+      order_samples <- samples
+    }
+
 
     # calculate barcode frequencies within samples
     barcode_freqs <-
@@ -43,6 +57,7 @@ plotBarcodesStackedBar <-
 
     # order barcodes based on maximum frequency across samples
     barcode_order <- barcode_freqs %>%
+      filter(sample %in% order_samples) %>%
       select(barcode, freq) %>%
       slice_max(
         order_by = freq,
@@ -81,10 +96,17 @@ plotBarcodesStackedBar <-
       setNames(as.list(cols_vector), unique(barcode_freqs$barcode))
 
     p <- barcode_freqs %>%
-      ggplot(aes(fill = barcode, x = sample, y = freq)) +
+      ggplot(aes(
+        fill = barcode,
+        x = sample,
+        y = freq,
+        alpha = ifelse(barcode %in% bc_labels, 1, 0.5)
+      )) +
       geom_bar(position = "stack", stat = "identity") +
+      scale_alpha_continuous(guide = FALSE, range = c(alpha_low_freq, 1)) +
       scale_fill_manual(values = label_values, breaks = bc_labels) +
       theme_bw() +
+      labs(fill = paste0("Top ", n_barcodes, " barcodes")) +
       ylab("Proportion") +
       theme(legend.text = element_text(size = 8),
             axis.text.x = element_text(angle = 45, hjust = 1))
