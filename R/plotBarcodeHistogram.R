4c9966f573daaaaed15a1cec87730de69f0dd2b9
Henrietta Holze <henrietta.holze@petermac.org>
2023-11-06 14:02:04 +1100

    replace plotBarcodeHistogram with plotBarcodeStackedBar

--- R/plotBarcodeHistogram.R
+++ R/plotBarcodeHistogram.R
@@ -1,69 +1,113 @@
-#' plotBarcodeHistogram
+#' plotBarcodesStackedBar
 #'
-#' Generate stacked barcode plots showing proportion from raw count object.
+#' Generate proportional stacked bar plot of barcodes from raw count object with n most frequent barcodes labelled.
 #'
 #' @param dgeObject DGEList object with barcode counts.
-#' @param sample Optional, name of sample to order all barcodes against.
-#' @param topN top n barcodes in sample to color, all other barcodes are shown in grey (integer). Default = `10`.
-#' @param title Plot title (string). Default = `Barcode Histogram`.
+#' @param topN Number of most frequent barcodes to show in legend (integer). Default = `10`.
+#' @param seedColors Seed for sampling colors (integer). Default = `1`.
+#' @param samples Samples to plot (vector of strings). Default all.
+#' @param orderSamples One or multiple samples to order barcodes by (string or vector of strings). Default all.
+#' @param alphaLowFreq Alpha of barcodes not in top n barcodes (decimal). Default = `1`.
 #'
-#' @return Returns a barcode histogram plot of barcodes represented by proportion of total pool
+#' @return Returns a stacked bar plot of barcode frequencies within samples
+#'
 #' @export
+#'
 #' @examples
-#' data(test.dge)
-#' plotBarcodeHistogram(test.dge, sample = "T0-1")
+#' plotBarcodesStackedBar(test.dge)
 
-plotBarcodeHistogram <-
+plotBarcodesStackedBar <-
   function(dgeObject,
-           sample = NULL,
            topN = 10,
-           name = "Barcode Histogram") {
+           seedColors = 1,
+           samples = NULL,
+           orderSamples = NULL,
+           alphaLowFreq = 1) {
 
-    if (methods::is(dgeObject)[1] != "DGEList") {
-      stop("Please supply a valid DGEList object as input")
+    inputChecks(dgeObject, samples = c(samples, orderSamples))
+
+    counts <- as.data.frame(dgeObject$counts)
+
+    if (is.null(samples)) {
+      samples <- colnames(counts)
     }
-    counts <- dgeObject$counts
 
-    # convert counts to a dataframe of proportions
-    barcodes.proportional <-
-      as.data.frame(sweep(counts, 2, colSums(counts), `/`) * 100)
+    counts <- counts[, samples]
 
-    if (!is.null(sample)) {
-      if (length(which(
-        as.character(sample) %in% colnames(barcodes.proportional)
-      )) != 1) {
-        stop("sample not found")
-      } else {
-        barcodes.proportional <-
-          barcodes.proportional[order(barcodes.proportional[, sample], decreasing = T), , drop = F]
+    if (!is.null(orderSamples)) {
+      if (!all(orderSamples %in% samples)) {
+        stop("samples to order by are not present or among selected samples")
       }
+    } else {
+      orderSamples <- samples
     }
 
-    top.bc <- utils::head(rowSums(barcodes.proportional), n = topN)
+    # calculate barcode frequencies within samples
+    barcode_freqs <-
+      counts %>%
+      as.data.frame() %>%
+      tibble::rownames_to_column(var = "barcode") %>%
+      pivot_longer(-barcode) %>%
+      dplyr::rename("count" = value, "sample" = name) %>%
+      group_by(sample) %>%
+      mutate(freq = count / sum(count)) %>%
+      ungroup()
 
-    # set colors
-    top.colors <- grDevices::rainbow(length(names(top.bc)))
-    all.colors <-
-      c(top.colors, rep("grey80", length(rownames(
-        barcodes.proportional
-      )) - length(top.colors)))
-    barcodes.proportional$color <- all.colors
+    # order barcodes based on maximum frequency across samples
+    barcode_order <- barcode_freqs %>%
+      filter(sample %in% orderSamples) %>%
+      dplyr::select(barcode, freq) %>%
+      group_by(barcode) %>%
+      slice_max(
+        order_by = freq,
+        n = 1,
+        with_ties = F
+      ) %>%
+      arrange(freq)
 
-    #par(mar=c(3,10,1.5,2) +.1)
-    graphics::barplot(
-      as.matrix(barcodes.proportional[, 1:length(names(barcodes.proportional)) -
-                                        1]),
-      beside = F,
-      horiz = T,
-      border = T,
-      col = barcodes.proportional$color,
-      names.arg = colnames(barcodes.proportional)[1:length(colnames(barcodes.proportional)) -
-                                                    1],
-      las = 1,
-      cex.names = .5,
-      cex.axis = 0.6,
-      xlab = "Barcode proportion (%)",
-      main = name
-    )
+    # set order of barcodes for plotting as factor levels
+    barcode_freqs$barcode <- factor(barcode_freqs$barcode,
+                                    levels = barcode_order$barcode)
 
+    qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual', ]
+    col_vector = unlist(mapply(
+      brewer.pal,
+      qual_col_pals$maxcolors,
+      rownames(qual_col_pals)
+    ))
+
+    n <- length(unique(barcode_freqs$barcode))
+
+    # map colors to barcodes
+    set.seed(seedColors)
+    cols_vector <- sample(col_vector, n, replace = T)
+
+    # set which barcodes to label
+    bc_labels <- barcode_freqs %>%
+      arrange(desc(freq)) %>%
+      pull(barcode) %>%
+      unique() %>%
+      head(topN) %>%
+      as.character()
+
+    label_values <-
+      setNames(as.list(cols_vector), unique(barcode_freqs$barcode))
+
+    p <- barcode_freqs %>%
+      ggplot(aes(
+        fill = barcode,
+        x = sample,
+        y = freq,
+        alpha = ifelse(barcode %in% bc_labels, 1, 0.5)
+      )) +
+      geom_bar(position = "stack", stat = "identity") +
+      scale_alpha_continuous(guide = FALSE, range = c(alphaLowFreq, 1)) +
+      scale_fill_manual(values = label_values, breaks = bc_labels) +
+      theme_bw() +
+      labs(fill = paste0("Top ", topN, " barcodes")) +
+      ylab("Proportion") +
+      theme(legend.text = element_text(size = 8),
+            axis.text.x = element_text(angle = 45, hjust = 1))
+
+    return(p)
   }
