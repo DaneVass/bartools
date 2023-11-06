8fe8e6cea84cfd295f9fc8250e9101429acc2160
Henrietta Holze <henrietta.holze@petermac.org>
2023-11-06 14:02:25 +1100

    add input check function

---
+++ R/utils.R
@@ -0,0 +1,57 @@
+
+
+# function to check if general inputs are valid
+inputChecks <-
+  function(dgeObject,
+           barcodes = NULL,
+           samples = NULL,
+           groups = NULL,
+           conditions = NULL) {
+    # check that input is DGEList object
+    if (methods::is(dgeObject)[1] != "DGEList") {
+      stop("Please supply a valid DGEList object as input")
+    }
+
+    # check if selected barcodes are present
+    if (!is.null(barcodes)) {
+      if (!all(barcodes %in% rownames(dgeObject))) {
+        stop(paste(
+          "Following barcodes are not present in the input DGEList object:",
+          paste0(barcodes[!barcodes %in% rownames(dgeObject)], collapse = ", ")
+        ))
+      }
+    }
+
+    # check if selected samples are present
+    if (!is.null(samples)) {
+      if (!all(samples %in% colnames(dgeObject))) {
+        stop(paste(
+          "Following samples are not in input data:",
+          paste0(samples[!samples %in% colnames(dgeObject)], collapse = ", ")
+        ))
+      }
+    }
+
+    # check if group is in samples metadata
+    if (!is.null(groups)) {
+      if (!all(groups %in% colnames(dgeObject$samples))) {
+        stop(paste(
+          "Following groups are not a column in the samples metadata:",
+          paste0(groups[!groups %in% colnames(dgeObject$samples)], collapse = ", ")
+        ))
+      }
+    }
+
+    # check if selected conditions are in group column
+    if (!is.null(conditions)) {
+      if (is.null(group)) {
+        stop("Specify column to group by if selecting conditions.")
+      }
+      if (!all(conditions %in% dgeObject$samples[[group]])) {
+        stop(paste(
+          "Following conditions are not in group column:",
+          paste0(conditions[!conditions %in% dgeObject$samples[[group]]], collapse = ", ")
+        ))
+      }
+    }
+  }
